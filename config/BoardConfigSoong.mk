bb17251f00035e7e40e826eb7ec33474cdceb31e
Pranav Vashi <neobuddy89@gmail.com>
2025-05-11 20:14:52 +0530

    soong: Add support for vendor tag with client package name to select
    * OEMs like OnePlus and Nothing detect camera package name to unlock features like 48mp.
    * Use TARGET_CAMERA_PACKAGE_NAME to specify the package name.
    
    Change-Id: I805af7258562ad177eb780b3622d2bc9af20f317
    Co-authored-by: Jake Weinstein <jake@aospa.co>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -35,6 +35,10 @@ ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)
     $(call soong_config_set,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
 endif
 
+ifneq ($(TARGET_CAMERA_PACKAGE_NAME),)
+    $(call soong_config_set,camera,package_name,$(TARGET_CAMERA_PACKAGE_NAME))
+endif
+
 # Charger
 lineage_charger_density := mdpi
 ifneq (,$(TARGET_SCREEN_DENSITY))
